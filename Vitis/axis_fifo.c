70b83bb35fdfb025ef19c3da3a106624905e24b2
Floyed_Shen@outlook.com <Floyed_Shen@outlook.com>
2020-02-28 19:24:11 +0800

    a stayalone PS part example

---
+++ Vitis/axis_fifo.c
@@ -0,0 +1,274 @@
+/***************************** Include Files *********************************/
+#include "xaxidma.h"
+#include "xparameters.h"
+#include "xdebug.h"
+#include "xfifo.h"
+
+#if defined(XPAR_UARTNS550_0_BASEADDR)
+#include "xuartns550_l.h"       /* to use uartns550 */
+#endif
+
+/******************** Constant Definitions **********************************/
+
+/*
+ * Device hardware build related constants.
+ */
+
+#define DMA_DEV_ID		XPAR_AXIDMA_0_DEVICE_ID
+
+#ifdef XPAR_AXI_7SDDR_0_S_AXI_BASEADDR
+#define DDR_BASE_ADDR		XPAR_AXI_7SDDR_0_S_AXI_BASEADDR
+#elif XPAR_MIG7SERIES_0_BASEADDR
+#define DDR_BASE_ADDR	XPAR_MIG7SERIES_0_BASEADDR
+#elif XPAR_MIG_0_BASEADDR
+#define DDR_BASE_ADDR	XPAR_MIG_0_BASEADDR
+#elif XPAR_PSU_DDR_0_S_AXI_BASEADDR
+#define DDR_BASE_ADDR	XPAR_PSU_DDR_0_S_AXI_BASEADDR
+#endif
+
+#ifndef DDR_BASE_ADDR
+#warning CHECK FOR THE VALID DDR ADDRESS IN XPARAMETERS.H, \
+		 DEFAULT SET TO 0x01000000
+#define MEM_BASE_ADDR		0x01000000
+#else
+#define MEM_BASE_ADDR		(DDR_BASE_ADDR + 0x1000000)
+#endif
+
+#define TX_BUFFER_BASE		(MEM_BASE_ADDR + 0x00100000)
+#define RX_BUFFER_BASE		(MEM_BASE_ADDR + 0x00300000)
+#define RX_BUFFER_HIGH		(MEM_BASE_ADDR + 0x004FFFFF)
+
+#define MAX_PKT_LEN		1024
+
+
+#define NUMBER_OF_TRANSFERS	10
+
+/**************************** Type Definitions *******************************/
+
+
+/***************** Macros (Inline Functions) Definitions *********************/
+
+
+/************************** Function Prototypes ******************************/
+
+#if (!defined(DEBUG))
+extern void xil_printf(const char *format, ...);
+#endif
+
+int XAxiDma_SimplePollExample(u16 DeviceId);
+static int CheckData(void);
+
+/************************** Variable Definitions *****************************/
+/*
+ * Device instance definitions
+ */
+XAxiDma AxiDma;
+XFifo xfifo;
+int *TxBufferPtr;
+int *RxBufferPtr;
+
+
+/*****************************************************************************/
+/**
+* The entry point for this example. It invokes the example function,
+* and reports the execution status.
+*
+* @param	None.
+*
+* @return
+*		- XST_SUCCESS if example finishes successfully
+*		- XST_FAILURE if example fails.
+*
+* @note		None.
+*
+******************************************************************************/
+int main()
+{
+	int Status;
+
+	xil_printf("\r\n--- Entering main() --- \r\n");
+
+	//init HLS IP
+	int _status = XFifo_Initialize(&xfifo,XPAR_FIFO_0_DEVICE_ID);
+	if(_status != XST_SUCCESS)
+	{
+		xil_printf("XExample_initialize failed\n");
+		return XST_FAILURE;
+	}
+
+	//start HLS IP
+	//XFifo_EnableAutoRestart(&xfifo);
+	XFifo_Start(&xfifo);
+
+	XFifo_Set_inc_V(&xfifo, 666);
+	int origin = XFifo_Get_inc_V(&xfifo);
+	xil_printf("Origin INC value: %d\r\n", origin);
+	/* Run the poll example for simple transfer */
+	Status = XAxiDma_SimplePollExample(DMA_DEV_ID);
+
+	if (Status != XST_SUCCESS) {
+		xil_printf("XAxiDma_SimplePoll Example Failed\r\n");
+		return XST_FAILURE;
+	}
+
+	xil_printf("Successfully ran XAxiDma_SimplePoll Example\r\n");
+
+	xil_printf("--- Exiting main() --- \r\n");
+
+	return XST_SUCCESS;
+
+}
+
+#if defined(XPAR_UARTNS550_0_BASEADDR)
+/*****************************************************************************/
+/*
+*
+* Uart16550 setup routine, need to set baudrate to 9600, and data bits to 8
+*
+* @param	None.
+*
+* @return	None
+*
+* @note		None.
+*
+******************************************************************************/
+static void Uart550_Setup(void)
+{
+
+	/* Set the baudrate to be predictable
+	 */
+	XUartNs550_SetBaud(XPAR_UARTNS550_0_BASEADDR,
+			XPAR_XUARTNS550_CLOCK_HZ, 9600);
+
+	XUartNs550_SetLineControlReg(XPAR_UARTNS550_0_BASEADDR,
+			XUN_LCR_8_DATA_BITS);
+
+}
+#endif
+
+/*****************************************************************************/
+/**
+* The example to do the simple transfer through polling. The constant
+* NUMBER_OF_TRANSFERS defines how many times a simple transfer is repeated.
+*
+* @param	DeviceId is the Device Id of the XAxiDma instance
+*
+* @return
+*		- XST_SUCCESS if example finishes successfully
+*		- XST_FAILURE if error occurs
+*
+* @note		None
+*
+*
+******************************************************************************/
+int XAxiDma_SimplePollExample(u16 DeviceId)
+{
+	XAxiDma_Config *CfgPtr;
+	int Status;
+	int Index;
+
+	TxBufferPtr = (int *)TX_BUFFER_BASE ;
+	RxBufferPtr = (int *)RX_BUFFER_BASE;
+
+	/* Initialize the XAxiDma device.
+	 */
+	CfgPtr = XAxiDma_LookupConfig(DeviceId);
+	if (!CfgPtr) {
+		xil_printf("No config found for %d\r\n", DeviceId);
+		return XST_FAILURE;
+	}
+
+	Status = XAxiDma_CfgInitialize(&AxiDma, CfgPtr);
+	if (Status != XST_SUCCESS) {
+		xil_printf("Initialization failed %d\r\n", Status);
+		return XST_FAILURE;
+	}
+
+	if(XAxiDma_HasSg(&AxiDma)){
+		xil_printf("Device configured as SG mode \r\n");
+		return XST_FAILURE;
+	}
+
+	/* Disable interrupts, we use polling mode
+	 */
+	XAxiDma_IntrDisable(&AxiDma, XAXIDMA_IRQ_ALL_MASK,
+						XAXIDMA_DEVICE_TO_DMA);
+	XAxiDma_IntrDisable(&AxiDma, XAXIDMA_IRQ_ALL_MASK,
+						XAXIDMA_DMA_TO_DEVICE);
+
+	//disable the cache
+    Xil_DCacheDisable();
+
+	for(Index = 0; Index < NUMBER_OF_TRANSFERS; Index ++) {
+		while(XFifo_IsDone(&xfifo)){
+			xil_printf("FIFO is busy\r\n");
+		}
+		XFifo_Start(&xfifo);
+		//write to the TxBuffer and RxBuffer default value
+		for(int i = 0; i < MAX_PKT_LEN; i++)
+		{
+				TxBufferPtr[i] = i + Index;
+				RxBufferPtr[i] = 0;
+		}
+
+		Status = XAxiDma_SimpleTransfer(&AxiDma,(UINTPTR) RxBufferPtr,
+					MAX_PKT_LEN*sizeof(int), XAXIDMA_DEVICE_TO_DMA);
+
+		if (Status != XST_SUCCESS) {
+			return XST_FAILURE;
+		}
+
+		Status = XAxiDma_SimpleTransfer(&AxiDma,(UINTPTR) TxBufferPtr,
+					MAX_PKT_LEN*sizeof(int), XAXIDMA_DMA_TO_DEVICE);
+
+		if (Status != XST_SUCCESS) {
+			return XST_FAILURE;
+		}
+
+		while ((XAxiDma_Busy(&AxiDma,XAXIDMA_DEVICE_TO_DMA)) ||
+			(XAxiDma_Busy(&AxiDma,XAXIDMA_DMA_TO_DEVICE))) {
+				/* Wait */
+		}
+
+		Status = CheckData();
+		if (Status != XST_SUCCESS) {
+			return XST_FAILURE;
+		}
+
+	}
+
+	/* Test finishes successfully
+	 */
+	return XST_SUCCESS;
+}
+
+/*****************************************************************************/
+/*
+*
+* This function checks data buffer after the DMA transfer is finished.
+*
+* @param	None
+*
+* @return
+*		- XST_SUCCESS if validation is successful.
+*		- XST_FAILURE otherwise.
+*
+* @note		None.
+*
+******************************************************************************/
+static int CheckData(void)
+{
+	xil_printf("\nThe TxData is\n");
+	for(int _index=0;_index < MAX_PKT_LEN;_index++)
+	{
+		xil_printf("%d ",TxBufferPtr[_index]);
+	}
+	xil_printf("\nThe RxData is\n");
+	for(int _index=0;_index < MAX_PKT_LEN;_index++)
+	{
+		xil_printf("%d ",RxBufferPtr[_index]);
+	}
+	xil_printf("\n\n");
+
+	return XST_SUCCESS;
+}
